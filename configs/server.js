083b604d26bef16ddc44fecec5b9e902153e0d0f
Diego Suarez-2022233 <dsuarez-2022233@kinal.edu.gt>
2024-03-10 15:52:44 -0600

    se hizo users

---
+++ configs/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
+import userRoutes from '../src/user/user.routes.js';
+
+export default class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+       
+        this.conectarDB();
+        this.middlewares();
+        this.routes();
+    }
+
+    conectarDB() {
+        // Conexión a la base de datos
+        dbConnection();
+    }
+
+    middlewares() {
+        // Configuración de middlewares
+        this.app.use(express.json());
+        this.app.use(cors());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    routes() {
+        // Configuración de rutas
+        this.app.use('/api/users', userRoutes); // Monta las rutas de usuario en la ruta base /api/users
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en el puerto ${this.port}`);
+        });
+    }
+}
---
+++ src/user/user.model.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['ADMIN', 'CLIENT'], default: 'CLIENT' },
+    // Puedes agregar más campos según tus necesidades
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
